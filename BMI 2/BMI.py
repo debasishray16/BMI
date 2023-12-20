74f1697d4b8faac6bd9565185b6145020ac21928
debasisray16 <debasishray3275@gmail.com>
2023-12-20 19:24:27 +0530

    New Part

--- BMI 2/BMI.py
+++ BMI 2/BMI.py
@@ -8,6 +8,11 @@ app.geometry("300x500")
 app.title("BMI CALCULATOR")
 app.config(bg="#020414")
 
+image_icon=PhotoImaege(file="")
+app.iconphoto(False,image_icon)
+
+top=Label(app,text="BMI CALCULATOR",font=('Arial',18,'bold'),fg="#FFFFFF",
+
 
 
 
