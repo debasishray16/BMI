8fd5aeaa32b3452a6cbc9713c05df579401566b3
debasisray16 <debasishray3275@gmail.com>
2023-12-22 21:10:02 +0530

    Bug Fixed

--- BMI 1/main.py
+++ BMI 1/main.py
@@ -4,9 +4,10 @@ from tkinter import ttk
 
 from PIL import Image, ImageTk
 
+
 root = Tk()
 root.title("BMI Calculator")
-root.geometry("470x580+300+200")
+root.geometry("600x600+300+200")
 root.resizable(False, False)
 root.configure(bg="#f0f1f5")
 
@@ -21,8 +22,8 @@ def BMI():
     label1.config(text=bmi)
 
     if (bmi <= 18.5):
-        label2.config(text="underweight!\n")
-        label3.config(text="You have lower weight.")
+        label2.config(text="underweight!")
+        label3.config(text="It indicates ,\n You have lower weight.")
 
     elif (bmi >= 18.5 and bmi <= 25):
         label2.config(text="Normal!")
@@ -47,22 +48,22 @@ root.iconphoto(False, image_icon)
 # top
 top = PhotoImage(file="BMI 1/images/top.png")
 top_image = Label(root, image=top, background="#f0f1f5")
-top_image.place(x=-10, y=-10)
+top_image.place(x=50, y=10)
 
 
 # bottom
-Label(root, width=72, height=18, bg="lightblue").pack(side=BOTTOM)
+Label(root, width=73, height=15, bg="lightblue").pack(side=BOTTOM)
 
 
 # two boxes
 box = PhotoImage(file="BMI 1/images/box.png")
-Label(root, image=box).place(x=20, y=100)
-Label(root, image=box).place(x=240, y=100)
+Label(root, image=box).place(x=50, y=100)
+Label(root, image=box).place(x=330, y=100)
 
 
 # scale
 scale = PhotoImage(file="BMI 1/images/scale.png")
-Label(root, image=scale, bg="lightblue").place(x=20, y=310)
+Label(root, image=scale, bg="lightblue").place(x=20, y=330)
 
 
 ####################################### Slider1#####################
@@ -113,7 +114,7 @@ style2 = ttk.Style()
 style2.configure("TScale", background="white")
 slider2 = ttk.Scale(root, from_=0, to=200, orient='horizontal',
                     style="TScale", command=slider_changed2, variable=current_value2)
-slider2.place(x=300, y=250)
+slider2.place(x=370, y=250)
 
 
 ################################################################
@@ -123,14 +124,14 @@ slider2.place(x=300, y=250)
 Height = StringVar()
 Weight = StringVar()
 
-height = Entry(root,  textvariable=Height, width=5, font='arial 50',
+height = Entry(root,  textvariable=Height, width=6, font='arial 30',
                bg="#fff", fg="#000", bd=0, justify=CENTER)  # to align text center.
-height.place(x=35, y=160)
+height.place(x=70, y=160)
 Height.set(get_current_value1())
 
-weight = Entry(root, textvariable=Weight, width=5, font='arial 50',
+weight = Entry(root, textvariable=Weight, width=6, font='arial 30',
                bg="#fff", fg="#000", bd=0, justify=CENTER)  # to align text center.
-weight.place(x=225, y=160)
+weight.place(x=350, y=160)
 Weight.set(get_current_value2())
 
 
@@ -140,16 +141,17 @@ secondimage.place(x=70, y=530)
 
 
 Button(root, text="View Report", width=15, height=2, font="arial 10 bold",
-       bg="#1f6e68", fg="white", command=BMI).place(x=280, y=340)
+       bg="#1f6e68", fg="white", command=BMI).place(x=430, y=530)
 
-label1 = Label(root, font="arial 60 bold", bg="lightblue", fg="#fff")
-label1.place(x=125, y=305)
+label1 = Label(root, font="arial 55 bold",
+               bg="lightblue", fg="#000000")
+label1.place(x=420, y=375)
 
 label2 = Label(root, font="arial 20 bold", bg="lightblue", fg="#3b3a3a")
-label2.place(x=280, y=430)
+label2.place(x=200, y=450)
 
 label3 = Label(root, font="arial 10 bold", bg="lightblue")
-label3.place(x=200, y=500)
+label3.place(x=185, y=500)
 
 
 root.mainloop()
